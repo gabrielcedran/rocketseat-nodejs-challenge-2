192afce7487a74d277fe07e7b9197cc6cc3ddf32
Gabriel Cedran <gabrielcedran@gmail.com>
2020-04-15 15:19:09 +0100

    Map Model Entities to reflect database structure

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,23 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn({ type: 'timestamp' })
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
