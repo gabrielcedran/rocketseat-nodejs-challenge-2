dc8958b375918d513e7148b728472e41d558a9f6
Gabriel Cedran <gabrielcedran@gmail.com>
2020-04-15 15:31:02 +0100

    Create transaction without any business logic

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,38 @@
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+
+  type: 'income' | 'outcome';
+
+  value: number;
+
+  category_id: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category_id,
+  }: RequestDTO): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    // fixme validate stuff
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category_id,
+    });
+    await transactionsRepository.save(transaction);
+    return transaction;
   }
 }
 
